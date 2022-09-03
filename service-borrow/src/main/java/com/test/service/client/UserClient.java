6abec056251d4e7a85bca95b77a0679ebf852a5a
Ocean <2080917477@qq.com>
2022-09-03 12:10:11 +0800

    openFegin 实现 服务发现,远程调用,负载均衡

---
+++ service-borrow/src/main/java/com/test/service/client/UserClient.java
@@ -0,0 +1,13 @@
+package com.test.service.client;
+
+import com.test.User;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@FeignClient("userservice")
+public interface UserClient {
+    
+    @RequestMapping("/user/{uid}")
+    User getUserById(@PathVariable("uid") int uid);
+}
