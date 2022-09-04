de292034cacc6f0c1f110099c0e41a4f5c571fd7
Ocean <2080917477@qq.com>
2022-09-04 13:26:25 +0800

    sentinel 异常处理

--- service-borrow/src/main/java/com/test/controller/BorrowController.java
+++ service-borrow/src/main/java/com/test/controller/BorrowController.java
@@ -1,5 +1,6 @@
 package com.test.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
 import com.alibaba.fastjson.JSONObject;
 import com.test.entity.UserBorrowDetail;
 import com.test.service.BorrowService;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import java.io.IOException;
 
 @RestController
 public class BorrowController {
@@ -33,5 +35,18 @@ public class BorrowController {
         object.put("success", false);
         object.put("massage", "您的请求频率过快，请稍后再试！");
         return object;
+    }
+
+    @RequestMapping("/test")
+    @SentinelResource(value = "test",
+            fallback = "except",    //fallback指定出现异常时的替代方案
+            exceptionsToIgnore = IOException.class)  //忽略那些异常，也就是说这些异常出现时不使用替代方案
+    String test(){
+        throw new RuntimeException("HelloWorld！");
+    }
+
+    //替代方法必须和原方法返回值和参数一致，最后可以添加一个Throwable作为参数接受异常
+    String except(Throwable t){
+        return t.getMessage();
     }
 }
