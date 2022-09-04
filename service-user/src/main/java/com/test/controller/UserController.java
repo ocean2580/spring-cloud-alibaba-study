e55bb6fe70636d05d1da7f9e71ac9d10f48815ab
Ocean <2080917477@qq.com>
2022-09-04 11:50:58 +0800

    sentinel安装部署

--- service-user/src/main/java/com/test/controller/UserController.java
+++ service-user/src/main/java/com/test/controller/UserController.java
@@ -4,7 +4,7 @@ package com.test.controller;
 import com.test.User;
 import com.test.service.UserService;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
+//import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,14 +14,14 @@ import javax.annotation.Resource;
 
 @RestController
 @RequestMapping("/user")
-@RefreshScope   //添加此注解就能实现自动刷新了
+//@RefreshScope   //添加此注解就能实现自动刷新了
 public class UserController {
 
     @Resource
     private UserService userService;
 
-    @Value("${text.txt}")
-    private String txt;
+//    @Value("${text.txt}")
+//    private String txt;
 
     @GetMapping("/{uid}")
     public User findUserById(@PathVariable("uid") int uid) {
@@ -29,10 +29,10 @@ public class UserController {
         return userService.getUserById(uid);
     }
 
-    @GetMapping("/txt")
-    public String getTxt() {
-
-        return this.txt;
-    }
+//    @GetMapping("/txt")
+//    public String getTxt() {
+//
+//        return this.txt;
+//    }
 
 }
